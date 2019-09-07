38d239c2292a3613fe977c59ab05a7d4de9ac4f7
Anay Gupta <guptaanay98@gmail.com>
2019-09-07 20:23:22 +0530

    2nd commit

---
+++ app/src/main/java/com/example/cardealershipmanagement/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.cardealershipmanagement;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewAnimator;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    ViewAnimator animator;
+    private static long Splash_Time = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logo);
+        animator = findViewById(R.id.animator2);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },Splash_Time);
+    }
+}

ee43d8fe747881863d5c7f5de1de80932bbd2537
Anay Gupta <guptaanay98@gmail.com>
2019-09-21 16:37:24 +0530

    ...

--- app/src/main/java/com/example/cardealershipmanagement/HomeActivity.java
+++ app/src/main/java/com/example/cardealershipmanagement/HomeActivity.java
@@ -2,18 +2,14 @@ package com.example.cardealershipmanagement;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,6 +65,10 @@ public class HomeActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id == R.id.logout) {
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -84,11 +84,14 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.add_emp) {
             Intent intent = new Intent(HomeActivity.this, NewEmployeeActivity.class);
             startActivity(intent);
+
         } else if (id == R.id.add_car) {
 
         } else if (id == R.id.update_car) {
 
         } else if (id == R.id.add_buyer) {
+            Intent intent = new Intent(HomeActivity.this, AddBuyerActivity.class);
+            startActivity(intent);
 
         } else if (id == R.id.sales_report) {
 
